fa54c2bf17c789d74a7377978a418ac339cb6930
CodedBeats <luca.haar@icloud.com>
2025-07-25 22:46:53 +1000

    feat(backend): setup simple express and axios backend with /api/jobs GET endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
-// server.js
-const helloWorld = "waaaaa";
+import express, { json } from 'express';
+import cors from 'cors';
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(json());
+
+// routes
+import jobRoutes from './routes/jobs.js';
+app.use('/api/jobs', jobRoutes);
+
+const PORT = 5001; // fifferent from Flask's 5002
+
+app.listen(PORT, () => console.log(`Node server running on port ${PORT}`));
