097ed983b04bd0d1d51d3f74d93ea22b992e8f35
CodedBeats <luca.haar@icloud.com>
2025-07-28 17:48:46 +1000

    fix(backend): catch and stop the double frontend to backend request

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -2,17 +2,32 @@
 import axios from "axios";
 
 
+// init map to remember recent requests
+const recentRequests = new Map();
+
 export const scrapeJobs = async (req, res) => {
     try {
         // get job title and location from request (in url)
         const { job_title, job_location } = req.query;
-
+        // guard
         if (!job_title) {
             return res.status(400).json({ error: "job_title required" });
         }
 
         console.log(`forwarding to scraper: [${job_title}] job in [${job_location || 'any location'}] location`);
 
+        // create key for recent requests
+        const key = `${job_title}|${job_location}`.toLowerCase();
+        // check if request has been made recently (within 30s)
+        const now = Date.now();
+        const existing = recentRequests.get(key);
+        if (existing && now - existing.timestamp < 30000) {
+            console.log(`duplicate request detected, ignoring: ${key}, (probably just dupe frontend request)`); 
+            return res.status(429).json({ error: "duplicate request too soon" });
+        }
+
+        // atore timestamp before forwarding
+        recentRequests.set(key, { timestamp: now });
 
         // call Flask scraper API
         const response = await axios.get("http://localhost:5002/scrape", {
