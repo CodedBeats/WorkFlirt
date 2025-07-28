d72627e3a6f9553b6f7751959c9e132068a7104e
CodedBeats <luca.haar@icloud.com>
2025-07-28 17:35:06 +1000

    fix(backend): comments clean up

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,15 +1,18 @@
+// dependencies
 import axios from "axios";
+
 
 export const scrapeJobs = async (req, res) => {
     try {
+        // get job title and location from request (in url)
         const { job_title, job_location } = req.query;
-        // console.log(job_title, job_location);
 
         if (!job_title) {
             return res.status(400).json({ error: "job_title required" });
         }
 
-        console.log(`forwarding to scraper: [${job_title}]job in [${job_location || 'any location'}]location`);
+        console.log(`forwarding to scraper: [${job_title}] job in [${job_location || 'any location'}] location`);
+
 
         // call Flask scraper API
         const response = await axios.get("http://localhost:5002/scrape", {
@@ -17,10 +20,11 @@ export const scrapeJobs = async (req, res) => {
                 job_title: job_title,
                 job_location: job_location || '',
             },
-            timeout: 60000 // 1 min timeout
+            timeout: 120000 // 2 min timeout for scraper to fetch and return jobs
         });
 
-        // (process data later)
+
+        // TODO > process data better
         const result = {
             ...response.data,
             processedAt: new Date().toISOString()
@@ -29,15 +33,18 @@ export const scrapeJobs = async (req, res) => {
         res.json(result);
         console.log("returning data to frontend from scraper");
 
+
     } catch (error) {
-        console.error('flask scraper api endpoint failed:', error.message);
+        console.error('scraper api failed:', error.message);
     
         if (error.code === 'ECONNABORTED') {
+            // didn't recieve a timely response from the scraper
             res.status(504).json({ error: "scraper service timeout" });
         } else if (error.response) {
-            // forward Flask error message
+            // other error status
             res.status(error.response.status).json(error.response.data);
         } else {
+            // web server encountered an unexpected condition that prevented it from fulfilling a client's request
             res.status(500).json({ error: "internal server error" });
         }
     }
